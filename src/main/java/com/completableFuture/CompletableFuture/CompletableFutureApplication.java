2dd2079e800a9ad55f12c66eeb2ae5c95f7d97ed
sagardas <sagardas.195@gmail.com>
2024-02-29 15:17:28 +0530

    first commit

---
+++ src/main/java/com/completableFuture/CompletableFuture/CompletableFutureApplication.java
@@ -0,0 +1,13 @@
+package com.completableFuture.CompletableFuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompletableFutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompletableFutureApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/completableFuture/CompletableFuture/async/MyCompletableFuture.java
@@ -0,0 +1,11 @@
+package com.completableFuture.CompletableFuture.async;
+
+/**
+ * Created by sagardas on 29/02/24.
+ */
+public class MyCompletableFuture {
+
+    public static void main(String[] args) {
+
+    }
+}

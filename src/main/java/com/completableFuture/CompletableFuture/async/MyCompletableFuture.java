a833a6b611aa61d140c3225fa5bf017c4cbcb9a1
sagardas <sagardas.195@gmail.com>
2024-02-29 16:01:11 +0530

    first commit

--- src/main/java/com/completableFuture/CompletableFuture/async/MyCompletableFuture.java
+++ src/main/java/com/completableFuture/CompletableFuture/async/MyCompletableFuture.java
@@ -1,12 +1,38 @@
 package com.completableFuture.CompletableFuture.async;
 
+import org.apache.catalina.Executor;
+
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
 /**
  * Created by sagardas on 29/02/24.
  */
 public class MyCompletableFuture {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        CompletableFuture<String> completableFuture=computeAsync();
+        System.out.println( completableFuture.get());
 
+    }
 
+    private static CompletableFuture<String> computeAsync() {
+
+        String[] str={"HR","MBA","SDE"};
+CompletableFuture<String> completableFuture=new CompletableFuture<>();
+        ExecutorService executorService= Executors.newFixedThreadPool(8);
+
+        executorService.submit(()->{
+
+                    String name= Arrays.stream(str).collect(Collectors.joining(",","[","]"));
+      completableFuture.complete(name);
+      return null;
+
+        });
+        return completableFuture;
     }
 }
